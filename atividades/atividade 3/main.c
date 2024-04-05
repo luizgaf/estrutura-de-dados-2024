afb2c60576656d791680a766b43044a190378cfd
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-04-05 09:00:12 -0300

    3.0

--- atividades/atividade 3/main.c
+++ atividades/atividade 3/main.c
@@ -1,28 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
 int main(int argc, char *argv[]){
-    int nums[5], i = 0, y;
+    if (argc != 2) {
+        printf("Uso: %s <nome_do_arquivo>\n", argv[0]);
+        return 1;
+    }
+    int i = 0, y;
+    int *nums = (int *)malloc(1000000 * sizeof(int));
+    if(nums == NULL){
+        printf("Falhou ao alocar memoria\n");
+        return 1;
+    }
+    
     FILE *arquivo_txt, *saida_txt;
 
-    arquivo_txt = fopen("arquivo.txt", "r");
+    arquivo_txt = fopen(argv[1], "r");
     if (arquivo_txt == NULL) {
         printf("erro ao abrir o arquivo\n");
         return 1;
     }
-    while(fscanf(arquivo_txt, "%i", &nums[i]) == 1){
+    while(fscanf(arquivo_txt, "%i", &nums[i]) == 1 && i < 1000000){
         i++;
     }
     fclose(arquivo_txt);
 
-    for (int j = 0; j < i - 1; j++) {
-        for (int k = 0; k < i - j - 1; k++) {
-            if (nums[k] > nums[k + 1]) {
-                y = nums[k];
-                nums[k] = nums[k + 1];
-                nums[k + 1] = y;
-            }
-        }
-    }
+    mergeSort(nums, 0, i-1);
 
     saida_txt = fopen("saida.txt","w");
     if (saida_txt == NULL) {
@@ -34,6 +86,7 @@ int main(int argc, char *argv[]){
     fprintf(saida_txt, "%i\n", nums[j]);
     }
     fclose(saida_txt);
+    free(nums);
 
     exit(0);
 
