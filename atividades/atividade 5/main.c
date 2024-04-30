4396e8c599a5f6f3d08da4e22f35c1c8c7dda7c7
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-04-30 14:44:50 -0300

    5

--- atividades/atividade 5/main.c
+++ atividades/atividade 5/main.c
@@ -6,20 +6,31 @@ int main(int argc, char* argv[]){
 
     No* H = no('A', NULL);
     inserir_no(H, 'B');
-    inserir_no(H, 'C');
+    inserir_no(H, 'Y');
+    inserir_no(H, 'K');
+    inserir_no(H, 'P');
+    inserir_no(H, 'Y');
+    inserir_no(H, 'K');
+    inserir_no(H, 'P');
+    inserir_no(H, 'Y');
+    inserir_no(H, 'K');
+    inserir_no(H, 'P');
+    inserir_no(H, 'Y');
     inserir_no(H, 'K');
     inserir_no(H, 'P');
 
     imprimir_lista(H);
-    printf("\n");
+    printf("\n\n");
 
+    printf("inseriondo no: L\n");
     inserir_no(H, 'L');
     imprimir_lista(H);
-    printf("\n");
+    printf("\n\n");
 
+    printf("removendo no: Y\n");
     remover_no(H, 'Y');
     imprimir_lista(H);
-    printf("\n");
+    printf("\n\n");
 
     liberar_lista(H);
     exit(0);
